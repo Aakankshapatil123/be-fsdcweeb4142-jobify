cf5df7ce747f1c8a79ae660c81a077ab870d494d
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-07-26 15:33:02 +0530

    isAuthenticated and allowRoles middlewares setup --done

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // import express
+const cookieParser = require("cookie-parser")
 const express = require("express");
 const authRouter = require("./routers/authRouters");
 
 // create express app
 const app = express();
+
+// parse cookies
+app.use(cookieParser());
 
 // parse the request body as JSON
 app.use(express.json())
