90ed4f6bcef431efad85ca46babe844d51eb0106
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-07-26 12:21:50 +0530

    register endpoind --done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,40 @@
+const User = require("../models/user")
+const bcrypt = require("bcrypt");
+const { SALT_ROUNDS } = require("../utils/config");
+
 const authController = {
     // register
     register: async (request, response) => {
        try{
-        return response.status(200).json({message: "register rout" })
-       }catch(e) {
+           // get name, email, password from request body
+           const { name, email, password } = request.body
+          
+           //   check if user with the same already exist in the databse
+            const existingUser =  await User.findOne({email});
+
+           //   ifyes, retun a 400 response with message "User already exists"
+            if(existingUser){
+               return response.status(500).json({message: "User already exists"})
+            }
+
+           // hash password using bcrypt
+           const hashPassword = await bcrypt.hash(password, parseInt(SALT_ROUNDS))
+           
+          //   create new user object using user model
+          const newUser = new User({
+            name,
+            email,
+            password:hashPassword
+          })
+ 
+           // save the user object to the database
+           await newUser.save();
+
+           // return a success responce
+           return response.status(200).json({message: "User register successfuly" })
+         }catch(e) {
         return response.status(500).json({message: "", error:e.message})
-       }
+      }
     },
     
     // login
--- utils/config.js
+++ utils/config.js
@@ -4,11 +4,13 @@ const MONGODB_URL = process.env.MONGODB_URL;
 const ENV = process.env.ENV
 const HOST= process.env.HOST
 const PORT= process.env.PORT
+const SALT_ROUNDS = process.env.SALT_ROUNDS
 
 
 module.exports = {
     MONGODB_URL,
     ENV,
     HOST,
-    PORT
+    PORT,
+    SALT_ROUNDS
 }
